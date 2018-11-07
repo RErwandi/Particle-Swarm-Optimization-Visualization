140463aa1942085ff7437108a6269f24bd1fd8ba
Reynold Erwandi <reynolderwandi@gmail.com>
2018-11-07 16:18:44 +0700

    Particle Swarm Optimization

---
+++ Assets/Scripts/ParticleSwarmOptimization.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSwarmOptimization : MonoBehaviour {
+
+    public GameObject swarmPrefab;
+    public float terrainSize;
+    public Material bestMaterial;
+    public Material normalMaterial;
+    public int particleCount;
+    public float cognitionRate;
+    public float socialRate;
+    public int maxIteration;
+    public float durationIteration;
+    public List<Swarm> swarms = new List<Swarm>();
+    public Swarm bestSwarm;
+    public float bestFitness = 0;
+
+	void Start () {
+		for(int i = 0; i < particleCount; i++)
+        {
+            Vector3 randomPosition = new Vector3(Random.Range(-terrainSize, terrainSize), 0, Random.Range(-terrainSize, terrainSize));
+            GameObject newSwarm = Instantiate(swarmPrefab, randomPosition, Quaternion.identity);
+            swarms.Add(newSwarm.GetComponent<Swarm>());
+        }
+
+        StartCoroutine(PSO());
+	}
+	
+
+	void Update () {
+		
+	}
+
+    IEnumerator PSO()
+    {
+        for(int i = 0; i < maxIteration; i++)
+        {
+            // Update All Swarms Fitness
+            foreach (Swarm s in swarms)
+            {
+                s.UpdateFitness();
+            }
+            
+            // Get Best Swarms and Best Fitness
+            if(bestSwarm != null)
+                bestSwarm.GetComponent<Renderer>().material = normalMaterial;
+            bestSwarm = GetBestSwarm();
+            bestSwarm.GetComponent<Renderer>().material = bestMaterial;
+            bestFitness = bestSwarm.best_fitness;
+
+            // Move all Swarms
+            foreach (Swarm s in swarms)
+            {
+                s.UpdateVelocity(bestSwarm, cognitionRate, socialRate, terrainSize, durationIteration);
+            }
+            yield return new WaitForSeconds(durationIteration);
+        }
+    }
+
+    Swarm GetBestSwarm()
+    {
+        Swarm bestSwarm = swarms[0];
+        float currentBestFit = bestSwarm.best_fitness;
+        foreach (Swarm s in swarms)
+        {
+            if (currentBestFit < s.best_fitness)
+            {
+                bestSwarm = s;
+                currentBestFit = s.best_fitness;
+            }
+        }
+        return bestSwarm;
+    }
+}
---
+++ Assets/Scripts/Swarm.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Swarm : MonoBehaviour {
+
+    public float offsetY;
+    public float best_fitness = -1;
+
+	void Start () {
+        
+	}
+
+	void Update () {
+        Vector3 CurrentPos = transform.position;
+        CurrentPos.y = Terrain.activeTerrain.SampleHeight(transform.position) + offsetY;
+        transform.position = CurrentPos;
+    }
+
+    public void UpdateFitness()
+    {
+        float fitness = Terrain.activeTerrain.SampleHeight(transform.position);
+        if(best_fitness < fitness || best_fitness == -1)
+            best_fitness = fitness;
+    }
+
+    public void UpdateVelocity(Swarm bestSwarm, float cognitiveRate, float socialRate, float bound, float duration)
+    {
+        Vector3 nextPosition = transform.position;
+        for (int i = 0; i < 2; i++)
+        {
+            float r1 = Random.Range(0f, 1f);
+            float r2 = Random.Range(0f, 1f);
+            if(i == 0)
+            {
+                float vCognitive = cognitiveRate * r1 * (bestSwarm.transform.position.x - transform.position.x);
+                float sCognitive = socialRate * r2 * (bestSwarm.transform.position.x - transform.position.x);
+                nextPosition.x = transform.position.x + vCognitive + sCognitive;
+                nextPosition.x = Mathf.Clamp(nextPosition.x, -bound, bound);
+            } else
+            {
+                float vCognitive = cognitiveRate * r1 * (bestSwarm.transform.position.z - transform.position.z);
+                float sCognitive = socialRate * r2 * (bestSwarm.transform.position.z - transform.position.z);
+                nextPosition.z = transform.position.z + vCognitive + sCognitive;
+                nextPosition.z = Mathf.Clamp(nextPosition.z, -bound, bound);
+            }
+        }
+        
+        transform.DOMoveX(nextPosition.x, duration);
+        transform.DOMoveZ(nextPosition.z, duration);
+    }
+}
